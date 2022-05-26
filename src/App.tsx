489b386ac9323dde4ad1c35ab3a65c530d12ae60
Anshu Singh <anshurajlive@gmail.com>
2022-05-26 18:38:08 +0530

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import * as React from "react";
 import {
   ChakraProvider,
   Box,
@@ -8,31 +8,64 @@ import {
   Code,
   Grid,
   theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+  Heading,
+  Avatar,
+  Center,
+  Image,
+  Flex,
+  Stack,
+  Button,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { Logo } from "./Logo";
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import WithSubnavigation from "./layouts/header";
+import { LargeWithAppLinksAndSocial } from "./layouts/footer";
+import SimpleCard from "./pages/login";
+import SignupCard from "./pages/signup";
+
+const Home = lazy(() => import("./pages/home"));
+
+const { PUBLIC_URL } = process.env;
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
+    <WithSubnavigation />
+    <BrowserRouter basename={PUBLIC_URL}>
+      <Suspense>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signin" component={SimpleCard} />
+          <Route exact path="/signup" component={SignupCard} />
+        </Switch>
+      </Suspense>
+    </BrowserRouter>
+    <LargeWithAppLinksAndSocial />
   </ChakraProvider>
-)
+);
+// <ChakraProvider theme={theme}>
+
+//   <Box textAlign="center" fontSize="xl">
+//     <Grid minH="100vh" p={3}>
+//       <ColorModeSwitcher justifySelf="flex-end" />
+//       <VStack spacing={8}>
+//         <Logo h="40vmin" pointerEvents="none" />
+//         <Text>
+//           Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+//         </Text>
+//         <Link
+//           color="teal.500"
+//           href="https://chakra-ui.com"
+//           fontSize="2xl"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn Chakra
+//         </Link>
+//       </VStack>
+//     </Grid>
+//   </Box>
+
+// </ChakraProvider>
---
+++ src/pages/home.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+  Heading,
+  Avatar,
+  Center,
+  Image,
+  Flex,
+  Stack,
+  Button,
+  useColorModeValue,
+  Switch,
+} from "@chakra-ui/react";
+import { ColorModeSwitcher } from "../ColorModeSwitcher";
+import { Logo } from "../Logo";
+import { LargeWithAppLinksAndSocial } from "../layouts/footer";
+import WithSubnavigation from "../layouts/header";
+import CaptionCarousel from "../components/slider";
+
+//import Main from '../layouts/Main';
+
+const Home = () => (
+  <ChakraProvider theme={theme}>
+    <CaptionCarousel />
+    {/* <WithSubnavigation /> */}
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <VStack spacing={8}>
+          <Logo h="40vmin" pointerEvents="none" />
+          <Text>
+            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+          </Text>
+          <Link
+            color="teal.500"
+            href="https://chakra-ui.com"
+            fontSize="2xl"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Chakra 1
+          </Link>
+        </VStack>
+      </Grid>
+    </Box>
+    {/* <LargeWithAppLinksAndSocial /> */}
+  </ChakraProvider>
+);
+
+export default Home;
